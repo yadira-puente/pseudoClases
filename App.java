607dcdd688e2457c919d7b86955dee82fc350914
yadira-puente <yadira.bridge@gmail.com>
2021-09-03 20:24:51 -0500

    :disabled:ejemplo 5: tiempo invertido 10 minutos

---
+++ App.java
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Adjunte el detector de eventos `change` al checkbox
+    document.getElementById('billing-checkbox').onchange = toggleBilling;
+  }, false);
+  
+  function toggleBilling() {
+    // Seleccione los campos de texto de facturación
+    var billingItems = document.querySelectorAll('#billing input[type="text"]');
+  
+    // Alternar los campos de texto de facturación
+    for (var i = 0; i < billingItems.length; i++) {
+      billingItems[i].disabled = !billingItems[i].disabled;
+    }
+  }
